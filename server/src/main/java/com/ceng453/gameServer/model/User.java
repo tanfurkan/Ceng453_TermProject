f76ae29fecf6615564398f27a4829d8b2905beba
ilyaseren.yilmaz <eren.yilmaz@metu.edu.tr>
2020-03-27 20:30:02 +0300

    Fields of User model are added.

--- server/src/main/java/com/ceng453/gameServer/model/User.java
+++ server/src/main/java/com/ceng453/gameServer/model/User.java
@@ -1,7 +1,20 @@
 package com.ceng453.gameServer.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
 }
