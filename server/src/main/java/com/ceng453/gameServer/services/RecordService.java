f43e9178b78c4ff205be650ba15d63e64448f567
furkan.tan <tan.furkan@metu.edu.tr>
2020-03-28 18:17:18 +0300

    Record Service
    Methods for Record Service are implemented.

---
+++ server/src/main/java/com/ceng453/gameServer/services/RecordService.java
@@ -0,0 +1,49 @@
+package com.ceng453.gameServer.services;
+
+import com.ceng453.gameServer.model.Record;
+import com.ceng453.gameServer.model.User;
+import com.ceng453.gameServer.repository.RecordRepository;
+import com.ceng453.gameServer.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class RecordService {
+
+    private final RecordRepository recordRepository;
+    private final UserRepository userRepository;
+
+    public void addRecord(Long userID, Long score) {
+        Optional<User> user = userRepository.findById(userID);
+        if(user.isPresent()){
+            Record record = new Record();
+            record.setUser(user.get());
+            record.setScore(score);
+            record.setDate(System.currentTimeMillis());
+            recordRepository.save(record);
+        }
+    }
+
+    public List<Record> getAllRecords() {
+
+        return recordRepository.findAll(Sort.by(Sort.Direction.DESC,"score"));
+    }
+
+    public List<Record> getMonthlyRecords() {
+
+        Long oneMonth = System.currentTimeMillis() - (long) (43200000); // (30 * 24 * 60 * 1000);
+        return recordRepository.findAllByDateGreaterThanEqualOrderByScoreDesc(oneMonth);
+    }
+
+    public List<Record> getWeeklyRecords() {
+
+        Long oneWeek = System.currentTimeMillis() - (long) (10080000); //(7 * 24 * 60 * 1000);
+        return recordRepository.findAllByDateGreaterThanEqualOrderByScoreDesc(oneWeek);
+    }
+
+}
