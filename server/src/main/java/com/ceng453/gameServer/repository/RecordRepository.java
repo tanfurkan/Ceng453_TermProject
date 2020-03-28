98de5212fd652b353e84473d70235c5306e56c5f
furkan.tan <tan.furkan@metu.edu.tr>
2020-03-29 02:24:59 +0300

    @Repository
    @Repository annotations are added to related interfaces.

--- server/src/main/java/com/ceng453/gameServer/repository/RecordRepository.java
+++ server/src/main/java/com/ceng453/gameServer/repository/RecordRepository.java
@@ -2,9 +2,11 @@ package com.ceng453.gameServer.repository;
 
 import com.ceng453.gameServer.model.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
     List<Record> findAllByDateGreaterThanEqualOrderByScoreDesc(Long date);
