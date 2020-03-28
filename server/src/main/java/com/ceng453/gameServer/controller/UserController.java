387ac832fd2b3027f977c1ea606f80ac59a7eba6
ilyaseren.yilmaz <eren.yilmaz@metu.edu.tr>
2020-03-29 02:06:18 +0300

    UserController and UserService is updated for Spring Security and JWT. Authentication System is implemented.

--- server/src/main/java/com/ceng453/gameServer/controller/UserController.java
+++ server/src/main/java/com/ceng453/gameServer/controller/UserController.java
@@ -5,6 +5,7 @@ import com.ceng453.gameServer.services.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,21 +20,19 @@ public class UserController {
 
     @PostMapping("/login")
     @ApiOperation(value = "Logins user with given username and password",
-                    notes = "Provide username and password to login",
-                    response = boolean.class )
-    public boolean login(@ApiParam(value = "User information that will be used for login action")
-                             @RequestBody User user)
-    {
+            notes = "Provide username and password to login",
+            response = ResponseEntity.class)
+    public ResponseEntity<?> login(@ApiParam(value = "User information that will be used for login action")
+                                   @RequestBody User user) throws Exception {
         return userService.login(user);
     }
 
     @GetMapping("/getUserID")
     @ApiOperation(value = "Gets ID of the user with given username",
             notes = "Provide username to receive ID of that user",
-            response = Long.class )
+            response = Long.class)
     public Long getUserID(@ApiParam(value = "Username of the user whose ID is wanted")
-            @RequestParam(value = "username") String username)
-    {
+                          @RequestParam(value = "username") String username) {
         return userService.getUserID(username);
     }
 
@@ -41,9 +40,8 @@ public class UserController {
     @GetMapping("/users")
     @ApiOperation(value = "Gets all users from database",
             notes = "Needs no parameter",
-            response = List.class )
-    public List<User> getAllUsers()
-    {
+            response = List.class)
+    public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
@@ -51,10 +49,9 @@ public class UserController {
     @GetMapping("/profile")
     @ApiOperation(value = "Gets the user from the provided ID",
             notes = "Provide ID to receive the user information",
-            response = User.class )
+            response = User.class)
     public User getUser(@ApiParam(value = "ID of the user that is wanted")
-            @RequestParam(value = "id") Long Id)
-    {
+                        @RequestParam(value = "id") Long Id) {
         return userService.getUser(Id);
     }
 
@@ -62,11 +59,10 @@ public class UserController {
     @PostMapping("/register")
     @ApiOperation(value = "Registers the user information given",
             notes = "Provide username and password that will be saved as new member",
-            response = String.class )
+            response = String.class)
     public String register(@ApiParam(value = "User information that will be registered")
-            @RequestBody User user
-    )
-    {
+                           @RequestBody User user
+    ) {
         return userService.register(user);
     }
 
@@ -74,12 +70,11 @@ public class UserController {
     @PutMapping("/profile")
     @ApiOperation(value = "Updates the user that is from given ID with new user information",
             notes = "Provide new password that will be updated",
-            response = String.class )
+            response = String.class)
     public String updateUser(@ApiParam(value = "User information that will be used to update user")
                              @RequestBody User user,
                              @ApiParam(value = "ID of the user that will be updated")
-                             @RequestParam(value = "id") Long Id)
-    {
+                             @RequestParam(value = "id") Long Id) {
         return userService.updateUser(user, Id);
     }
 
@@ -87,10 +82,9 @@ public class UserController {
     @DeleteMapping("/profile")
     @ApiOperation(value = "Delete the user with given ID",
             notes = "Provide ID of the user that will be deleted",
-            response = String.class )
+            response = String.class)
     public String deleteUser(@ApiParam(value = "ID of the user that will be deleted")
-                                 @RequestParam(value = "id") Long Id)
-    {
+                             @RequestParam(value = "id") Long Id) {
         return userService.deleteUser(Id);
     }
 
--- server/src/main/java/com/ceng453/gameServer/services/UserService.java
+++ server/src/main/java/com/ceng453/gameServer/services/UserService.java
@@ -1,8 +1,16 @@
 package com.ceng453.gameServer.services;
 
+import com.ceng453.gameServer.model.JwtResponse;
 import com.ceng453.gameServer.model.User;
 import com.ceng453.gameServer.repository.UserRepository;
+import com.ceng453.gameServer.util.JwtUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,22 +21,36 @@ import java.util.Optional;
 @AllArgsConstructor
 public class UserService {
 
+    private final AuthenticationManager authenticationManager;
+
+    private final UsersDetailsService usersDetailsService;
+
+    private final JwtUtil jwtTokenUtil;
+
+    private final PasswordEncoder passwordEncoder;
+
     private final UserRepository userRepository;
 
-    public boolean login(User requestUser)
-    {
-        Optional<User> user = userRepository.findByUsername(requestUser.getUsername());
-        if (user.isEmpty()) {
-            return false;
+    public ResponseEntity<?> login(User requestUser) throws Exception {
+        try {
+
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(requestUser.getUsername(), requestUser.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
         }
-        if (user.get().isDeleted()){
-            return false;
-        }
-        return user.get().getPassword().equals(requestUser.getPassword());
+
+        final UserDetails userDetails = usersDetailsService
+                .loadUserByUsername(requestUser.getUsername());
+
+        //Creating authentication jwt token
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(jwt));
+
     }
 
-    public Long getUserID(String username)
-    {
+    public Long getUserID(String username) {
         Optional<User> user = userRepository.findByUsername(username);
         if (user.isEmpty()) {
             return (long) -1;
@@ -36,37 +58,35 @@ public class UserService {
         return user.get().getId();
     }
 
-    public List<User> getAllUsers()
-    {
+    public List<User> getAllUsers() {
         return new ArrayList<>(userRepository.findAll());
     }
 
-    public User getUser(Long Id)
-    {
-        if(userRepository.findById(Id).isPresent())
+    public User getUser(Long Id) {
+        if (userRepository.findById(Id).isPresent())
             return userRepository.findById(Id).get();
         else
             return null;
     }
 
-    public String register(User user)
-    {
-        if(user.getUsername().isEmpty())
+    public String register(User user) {
+
+        if (user.getUsername().isEmpty())
             return "Username cannot be empty.";
-        if(user.getPassword().isEmpty())
+        if (user.getPassword().isEmpty())
             return "Password cannot be empty.";
-        if(userRepository.findByUsername(user.getUsername()).isPresent())
+        if (userRepository.findByUsername(user.getUsername()).isPresent())
             return "Username is already taken.";
         else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             userRepository.save(user);
-            return "User created successfully";
+            return "User created successfully. Please log in";
         }
         //TODO add more secure password checking system
     }
 
-    public String updateUser(User requestUser, Long Id)
-    {
-        if(userRepository.findById(Id).isPresent()) {
+    public String updateUser(User requestUser, Long Id) {
+        if (userRepository.findById(Id).isPresent()) {
             User user = userRepository.findById(Id).get();
             user.setPassword(requestUser.getPassword());
             userRepository.save(user);
@@ -76,9 +96,8 @@ public class UserService {
         return "User is not updated, please check the input fields and id.";
     }
 
-    public String deleteUser(Long Id)
-    {
-        if(userRepository.findById(Id).isPresent()) {
+    public String deleteUser(Long Id) {
+        if (userRepository.findById(Id).isPresent()) {
             User user = userRepository.findById(Id).get();
             user.setDeleted(true);
             userRepository.save(user);
@@ -86,4 +105,5 @@ public class UserService {
         }
         return "User is not deleted, please check the id.";
     }
+
 }
