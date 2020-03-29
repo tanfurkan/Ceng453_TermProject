9de5dfd0b2349c2de38555c86d5c1ef55ac9d5fd
furkan.tan <tan.furkan@metu.edu.tr>
2020-03-29 15:02:36 +0300

    New Queries - RecordRepository
    New queries are implemented in RecordRepository for enhanced performance and more flexibility.

--- server/src/main/java/com/ceng453/gameServer/repository/RecordRepository.java
+++ server/src/main/java/com/ceng453/gameServer/repository/RecordRepository.java
@@ -1,7 +1,11 @@
 package com.ceng453.gameServer.repository;
 
+import com.ceng453.gameServer.dao.RecordDAO;
 import com.ceng453.gameServer.model.Record;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +13,15 @@ import java.util.List;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
-    List<Record> findAllByDateGreaterThanEqualOrderByScoreDesc(Long date);
+    @Query( "SELECT record.user.username, record.score, record.date " +
+            "FROM Record record " +
+            "ORDER BY record.score DESC ")
+    List<Object []> findAllRecords(Pageable pageable);
+
+    @Query( "SELECT record.user.username, record.score, record.date " +
+            "FROM Record record " +
+            "WHERE record.date <= :date " +
+            "ORDER BY record.score DESC ")
+    List<Object []> findAllRecordsBefore(@Param("date") Long date, Pageable pageable);
 
 }
