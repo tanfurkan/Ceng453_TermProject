07e6fb70529ce8a25b19ceffad10885dfa323480
ilyaseren.yilmaz <eren.yilmaz@metu.edu.tr>
2020-03-29 19:21:33 +0300

    Refactoring and ER Diagram is added.

--- server/src/main/java/com/ceng453/gameServer/services/UserService.java
+++ server/src/main/java/com/ceng453/gameServer/services/UserService.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
