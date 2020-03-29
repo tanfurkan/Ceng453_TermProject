67e7ede4a9b32338aff3212971ebf16a86017576
ilyaseren.yilmaz <eren.yilmaz@metu.edu.tr>
2020-03-29 18:24:15 +0300

    JwtResponse Swagger Explanation is added.

--- server/src/main/java/com/ceng453/gameServer/model/JwtResponse.java
+++ server/src/main/java/com/ceng453/gameServer/model/JwtResponse.java
@@ -1,5 +1,7 @@
 package com.ceng453.gameServer.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -7,6 +9,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@ApiModel(description = "Details about the JWTResponse to login action.")
 public class JwtResponse {
+
+    @ApiModelProperty(notes = "The JWT Token")
     private final String jwt;
 }
