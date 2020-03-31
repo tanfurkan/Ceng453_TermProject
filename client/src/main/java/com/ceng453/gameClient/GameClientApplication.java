c6c43c024b1fe9690cea49608d9e893651f62524
furkan.tan <tan.furkan@metu.edu.tr>
2020-03-31 17:47:33 +0300

    Spring Project Created for Client
    Client part  of the project bootstrapped using Spring Initializr

---
+++ client/src/main/java/com/ceng453/gameClient/GameClientApplication.java
@@ -0,0 +1,13 @@
+package com.ceng453.gameClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameClientApplication.class, args);
+	}
+
+}
