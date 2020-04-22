092c83c1db0a0b049205ffb3edc303cb3eeee9ef
furkan.tan <tan.furkan@metu.edu.tr>
2020-04-23 02:07:12 +0300

    Login Page Completed
    Implementation of the Login Page is completed. Now, it is fully functional.

--- client/src/main/java/com/ceng453/gameClient/constants/ErrorConstants.java
+++ client/src/main/java/com/ceng453/gameClient/constants/ErrorConstants.java
@@ -2,4 +2,5 @@ package com.ceng453.gameClient.constants;
 
 public class ErrorConstants {
     public static final String NETWORK_ERROR = "Error: Please check network connection and try to login again.";
+    public static final String EMPTY_ERROR = "Error: Username or Password can not be empty.";
 }
--- client/src/main/java/com/ceng453/gameClient/scenes/LoginScreen.java
+++ client/src/main/java/com/ceng453/gameClient/scenes/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.ceng453.gameClient.scenes;
 
+import com.ceng453.gameClient.constants.ErrorConstants;
 import com.ceng453.gameClient.constants.SceneConstants;
 import com.ceng453.gameClient.controller.LoginController;
 import javafx.geometry.Insets;
@@ -12,45 +13,79 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+
+import static javafx.scene.text.TextAlignment.CENTER;
 
 public class LoginScreen {
 
-    private static GridPane root = new GridPane();
-    private static LoginController loginController = new LoginController();
+    private static final GridPane root = new GridPane();
+    private static final LoginController loginController = new LoginController();
 
-    public static Scene createContent(){
+    public static Scene createContent() {
 
-        root.setAlignment(Pos.CENTER);
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col1.setPercentWidth(20);
+        col2.setPercentWidth(30);
+        root.getColumnConstraints().addAll(col1, col2);
+
+        root.setAlignment(Pos.TOP_CENTER);
+
         root.setHgap(10);
-        root.setVgap(10);
-        root.setPadding(new Insets(25, 25, 25, 25));
+        root.setVgap(20);
+        root.setPadding(new Insets(SceneConstants.WINDOW_HEIGHT / 4.0 - 38, 25, 25, 25));
 
-        Label userName = new Label("User Name:");
+        /*  To Get Username */
+        Label userName = new Label("Username");
+        userName.setFont(Font.font("Verdana", 20));
+        userName.setTextFill(Color.WHITE);
+        userName.setPadding(new Insets(0, 0, 0, 20));
         root.add(userName, 0, 1);
 
-        TextField userTextField = new TextField();
-        root.add(userTextField, 1, 1);
+        TextField userNameTextField = new TextField();
+        root.add(userNameTextField, 1, 1);
 
-        Label pw = new Label("Password:");
-        root.add(pw, 0, 2);
+        /*  To Get Password */
+        Label password = new Label("Password");
+        password.setFont(Font.font("Verdana", 20));
+        password.setTextFill(Color.WHITE);
+        password.setPadding(new Insets(0, 0, 0, 20));
+        root.add(password, 0, 2);
 
         PasswordField passwordField = new PasswordField();
         root.add(passwordField, 1, 2);
 
-        Button signUpButton = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(signUpButton);
-        root.add(hbBtn, 1, 4);
 
-        final Text actiontarget = new Text();
-        root.add(actiontarget, 1, 6);
+        Button signInButton = new Button("Sign In");
+        Button signUpButton = new Button("Sign Up");
+
+        HBox hBoxButtons = new HBox(15);
+        hBoxButtons.setAlignment(Pos.BOTTOM_CENTER);
+        hBoxButtons.getChildren().add(signInButton);
+        hBoxButtons.getChildren().add(signUpButton);
+
+        root.add(hBoxButtons, 1, 3);
+
+        /* To Inform User */
+        final Text errorMessage = new Text();
+        errorMessage.setWrappingWidth(SceneConstants.WINDOW_WIDTH / 4.0);
+        errorMessage.setFill(Color.web(SceneConstants.MENU_COLOR, 0.6));
+        errorMessage.setTextAlignment(CENTER);
+        root.add(errorMessage, 1, 4);
+
+        signInButton.setOnAction(e -> {
+            if (userNameTextField.getText().length() < 1 || passwordField.getText().length() < 1)
+                errorMessage.setText(ErrorConstants.EMPTY_ERROR);
+            else {
+                String response = loginController.login(userNameTextField.getText(), passwordField.getText());
+                errorMessage.setText(response);
+            }
+        });
 
         signUpButton.setOnAction(e -> {
-            String response = loginController.login(userTextField.getText(), passwordField.getText());
-            actiontarget.setFill(Color.web(SceneConstants.MENU_COLOR, 0.6));
-            actiontarget.setText(response);
+            SceneConstants.stage.setScene(SignUpPage.createContent());
         });
 
         addTitle();
@@ -70,8 +105,12 @@ public class LoginScreen {
     }
 
     private static void addTitle() {
-        Title title = new Title("Space Shooter");
-        root.getChildren().add(title);
+        Title title = new Title("Space Shooter"); // TODO MAKE THE NAME CONSTANT
+        HBox hbBtn = new HBox(15);
+        hbBtn.setAlignment(Pos.BOTTOM_LEFT);
+        hbBtn.setPadding(new Insets(0, 0, 50, 30));
+        hbBtn.getChildren().add(title);
+        root.getChildren().add(hbBtn);
     }
 
 }
