4bbc45823b52275e5c7938dd12fc4d212068e69a
ilyaseren.yilmaz <eren.yilmaz@metu.edu.tr>
2020-04-22 18:19:55 +0300

    SceneConstants class is created.

--- client/src/main/java/com/ceng453/gameClient/GameClientApplication.java
+++ client/src/main/java/com/ceng453/gameClient/GameClientApplication.java
@@ -1,10 +1,12 @@
 package com.ceng453.gameClient;
 
+import com.ceng453.gameClient.constants.SceneConstants;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class GameClientApplication extends Application {
     @Override
     public void start(Stage stage) {
+        SceneConstants.stage = stage;
     }
 }
---
+++ client/src/main/java/com/ceng453/gameClient/constants/SceneConstants.java
@@ -0,0 +1,7 @@
+package com.ceng453.gameClient.constants;
+
+import javafx.stage.Stage;
+
+public class SceneConstants {
+    public static Stage stage;
+}
