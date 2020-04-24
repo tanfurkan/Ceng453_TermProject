92bfcf7f436c63b31ae711141310e145b5af1dfd
ilyaseren.yilmaz <eren.yilmaz@metu.edu.tr>
2020-04-24 17:40:34 +0300

    Wrong jwtToken format saved is fixed.

--- client/src/main/java/com/ceng453/gameClient/controller/LoginController.java
+++ client/src/main/java/com/ceng453/gameClient/controller/LoginController.java
@@ -5,24 +5,25 @@ import com.ceng453.gameClient.constants.NetworkConstants;
 import com.ceng453.gameClient.constants.SceneConstants;
 import com.ceng453.gameClient.scenes.MainMenuScreen;
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 
 public class LoginController{
     public String login(String username, String password){
         try {
-            HttpResponse<String> response
+            HttpResponse<JsonNode> response
                     = Unirest.post(NetworkConstants.API + "api/login")
                     .header("Content-Type","application/json")
                     .header("accept","application/json")
                     .body("{\"username\":\"" + username + "\", \"password\":\"" + password + "\"}")
-                    .asString();
+                    .asJson();
             if(response.getStatus() == 200) {
-                NetworkConstants.jwtToken = response.getBody();
+                NetworkConstants.jwtToken = response.getBody().getObject().getString("jwt");
                 SceneConstants.stage.setScene(MainMenuScreen.createContent());
                 return "";
             }
             else{
-                return response.getBody();
+                return response.getBody().toString();
             }
         }
         catch (Exception e){
