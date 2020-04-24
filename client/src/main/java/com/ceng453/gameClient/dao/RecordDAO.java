66f0a9c1d19e1f74870823fbac5e3e0e697a61a8
ilyaseren.yilmaz <eren.yilmaz@metu.edu.tr>
2020-04-24 17:22:41 +0300

    RecordDAO is added. gameObjects is deleted.

---
+++ client/src/main/java/com/ceng453/gameClient/dao/RecordDAO.java
@@ -0,0 +1,37 @@
+package com.ceng453.gameClient.dao;
+
+public class RecordDAO {
+    /**
+     * The username of the record owner
+     */
+    private String username;
+
+    /**
+     * Score of the user
+     */
+    private Long score;
+
+    /**
+     * Time of the record creation
+     */
+    private Long date;
+
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String newName){
+        username = newName;
+    }
+    public Long getScore(){
+        return score;
+    }
+    public void setScore(Long newScore){
+        score = newScore;
+    }
+    public Long getDate(){
+        return date;
+    }
+    public void setDate(Long newDate){
+        date = newDate;
+    }
+}
