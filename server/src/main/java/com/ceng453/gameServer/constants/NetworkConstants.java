f4368c10d1aff0878b0993f4def628aea0a29b72
furkan.tan <tan.furkan@metu.edu.tr>
2020-06-03 16:38:27 +0300

    Deployment Changes
    Deployment Changes

--- server/src/main/java/com/ceng453/gameServer/constants/NetworkConstants.java
+++ server/src/main/java/com/ceng453/gameServer/constants/NetworkConstants.java
@@ -1,7 +1,7 @@
 package com.ceng453.gameServer.constants;
 
 public class NetworkConstants {
-    public static int LISTEN_PORT = 8084;
+    public static int LISTEN_PORT = 8081;
     public static String START_SIGNAL = "S";
     public static String GAME_END_SIGNAL = "E";
 
